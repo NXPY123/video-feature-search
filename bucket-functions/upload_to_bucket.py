867136d24ae6833875147e1b6aea13efab6ecdbc
Neeraj P Yetheendran <neeraj.yathy@gmail.com>
2023-01-29 10:36:20 +0530

    added thumbnail generation

--- bucket-functions/upload_to_bucket.py
+++ bucket-functions/upload_to_bucket.py
@@ -37,4 +37,24 @@ def upload_blob_from_stream(bucket_name, file_name, destination_blob_name):
 
 # [END storage_stream_file_upload]
 
-upload_blob_from_stream("video-upload-bucket", "/Users/neeraj_py/Desktop/class/Sem3/ISC211/upload_to_bucket/funny_cat.mp4", "funny_cat.mp4")
+#upload_blob_from_stream("video-upload-bucket", "/Users/neeraj_py/Desktop/class/Sem3/ISC211/upload_to_bucket/funny_cat.mp4", "funny_cat.mp4")
+import subprocess
+def generate_thumbnail(video_input_path, img_output_path,bucket_name,destination_blob_name):
+    subprocess.call(['ffmpeg', '-i', video_input_path, '-ss', '00:00:00.000', '-vframes', '1', img_output_path])
+    storage_client = storage.Client.from_service_account_json('/Users/neeraj_py/Desktop/class/Sem3/ISC211/upload_to_bucket/civic-axon-123.json')
+
+    #storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    # Rewind the stream to the beginning. This step can be omitted if the input
+    # stream will always be at a correct position.
+
+
+    # Upload data from the stream to your bucket.
+    blob.upload_from_filename(img_output_path)
+
+#generate_thumbnail("/Users/neeraj_py/Desktop/class/Sem3/ISC211/upload_to_bucket/funny_cat.mp4", "/Users/neeraj_py/Desktop/class/Sem3/ISC211/upload_to_bucket/funny_cat.jpg","video-thumbnail-bucket","funny_cat.jpg")
+
+
+
